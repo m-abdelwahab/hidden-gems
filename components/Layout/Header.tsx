f4dff24f85e264052751132ab56fd52effe3c5b3
Mahmoud Abdel Wahab <mahmoud3799@gmail.com>
2021-01-08 16:09:17 +0200

    change header name

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">Habit Tracker</span>
+            <span className="ml-3 text-xl">Hidden Gems</span>
           </a>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
